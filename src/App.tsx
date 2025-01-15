213bc15fe19ed6dc8f6e04fe56606c32ded199df
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2025-01-15 19:31:23 +0530

    reame file ext from js to jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Suspense } from "react";
+import MainRoutes from "./router/MainRoutes";
 
-function App() {
-  const [count, setCount] = useState<number>(0)
+import Layout from "./components/Layout";
+import { Switch, BrowserRouter, useLocation } from "react-router-dom";
+import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js";
+import { ViewportProvider } from "./context/ViewportContext";
+import "./App.css";
+import "./responsive.css";
+// redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+const App: React.FC = () => {
+  return (
+    <ViewportProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Main />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </ViewportProvider>
+  );
+};
+
+const Main: React.FC = () => {
+  const location = useLocation();
+  // const isHome = false;
+  const isHome = location.pathname === "/";
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className='text-blue-500'>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Suspense fallback={null}>
+      <Layout hideHeader={isHome} hideFooter={isHome}>
+        <MainRoutes />
+      </Layout>
+    </Suspense>
+  );
+};
 
-export default App
+export default App;
